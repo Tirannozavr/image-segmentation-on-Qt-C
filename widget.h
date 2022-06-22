b8480115898dc45e863b0044ece8ddb6ea12251e
Tirannozavr <93453302+Tirannozavr@users.noreply.github.com>
2022-06-22 13:36:06 +0300

    formatting

--- widget.h
+++ widget.h
@@ -4,23 +4,23 @@
 #include <QWidget>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Widget; }
+namespace Ui {
+class Widget;
+}
 QT_END_NAMESPACE
 
-class Widget : public QWidget
-{
-    Q_OBJECT
+class Widget : public QWidget {
+  Q_OBJECT
 
-public:
-    Widget(QWidget *parent = nullptr);
-    ~Widget();
+ public:
+  Widget(QWidget *parent = nullptr);
+  ~Widget();
 
-private slots:
-    void on_pushButton_clicked();
-    void on_pushButton_2_clicked();
+ private slots:
+  void on_pushButton_clicked();
+  void on_pushButton_2_clicked();
 
-
-private:
-    Ui::Widget *ui;
+ private:
+  Ui::Widget *ui;
 };
-#endif // WIDGET_H
+#endif  // WIDGET_H
